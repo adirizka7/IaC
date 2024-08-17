312b3e6e3875c8df05fb084b59c9c541f4a168e3
Adi Rizka <26130158+adirizka7@users.noreply.github.com>
2024-08-17 08:18:54 +0700

    feat: update path, method, and api key requirement

--- lambda/apigateway.tf
+++ lambda/apigateway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
+  path_part   = "convert"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
@@ -11,9 +11,9 @@ resource "aws_api_gateway_resource" "resource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id      = aws_api_gateway_rest_api.api.id
   resource_id      = aws_api_gateway_resource.resource.id
-  http_method      = "GET"
+  http_method      = "POST"
   authorization    = "NONE"
-  api_key_required = true
+  api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "integration" {
